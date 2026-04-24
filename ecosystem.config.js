384a0926052236cfe9886326f87d3be05bb9f4ab
Trần Trung Kiên <trungkien2981412@gmail.com>
2026-04-24 18:53:06 +0700

    Setup CI/CD and PM2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  apps: [
+    {
+      name: 'daugia-frontend',
+      script: 'npm',
+      args: 'run start -- -p 1234',
+      cwd: '/var/www/web-thong-ke-dau-gia',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      }
+    },
+    {
+      name: 'daugia-backend',
+      script: 'npm',
+      args: 'start',
+      cwd: '/var/www/web-thong-ke-dau-gia/bot-crawls-data',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      }
+    }
+  ]
+};
